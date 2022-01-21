4689aee992baf45bbe998d1b77b9d3fc3b54fcf4
LancereReal <77140100+LancereReal@users.noreply.github.com>
2022-01-20 21:58:05 -0600

    Create OldReply.plugin.js

---
+++ OldReply.plugin.js
@@ -0,0 +1,24 @@
+/**
+ * @name OldReply
+ * @author Ham
+ * @description Allows you to reply to a message using the old method.
+ * @version 1.0.0
+ * @authorId 565673576477360129
+ * @authorLink https://github.com/Niemiets
+ * @website https://github.com/Niemiets/BD_Plugins
+ * @source https://github.com/Niemiets/BD_Plugins/tree/main/DashToSpaceInChannelName
+ * @updateUrl https://raw.githubusercontent.com/Niemiets/BD_Plugins/main/DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
+ */
+
+ module.exports = (_ => {
+	const config = {
+		"info": {
+			"name": "OldReply",
+			"author": "Ham",
+			"version": "1.0.0",
+			"description": "Allows you to reply to a message using the old method."
+		},
+		"Release": {
+			"Released": "The plugin has been released :D" 
+		}
+	}})
